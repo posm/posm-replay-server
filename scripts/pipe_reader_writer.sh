c99ca71e6254c1346df0b1deb35425c830d0f96d
bewakes <bewakepandey@gmail.com>
2019-11-29 17:14:34 +0545

    Update pipe reader script to read from pipe

--- scripts/pipe_reader_writer.sh
+++ scripts/pipe_reader_writer.sh
@@ -6,11 +6,11 @@ rm osmosis_command_reader.fifo 2>/dev/null
 mkfifo osmosis_command_reader.fifo
 
 
-while read -r cmd
+while read cmd
 do
     $cmd 2> /tmp/command_reader_error;
     # Write to another named pipe 'osmosis_result_reader' which is created by python app
     # NOTE: the following outputs multiline errors into single, which is desired
     # at the moment
     echo $? `cat /tmp/command_reader_error` > osmosis_result_reader.fifo;
-done
+done < osmosis_command_reader.fifo
